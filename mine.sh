57a15a1d614e1dc8f25713d3a9e5f95f030c5d84
Demontager <demontager@gmail.com>
2014-04-11 19:08:03 +0300

    Added notification to E-mail

--- mine.sh
+++ mine.sh
@@ -29,6 +29,14 @@ config=$(cat << 'EOF'
 ]
 EOF
 )
+# 3. Enable email notification ? YES/NO, default: NO
+notify="YES"
+
+# 4. Define mail settings to get notified. Leave smpt and port as default if gmail used.
+email="your_id@gmail.com"
+password="your_email_password"
+smtp="smtp.gmail.com"
+port="587"
 #***************Configuration END**************************************#
 
 bamt() {
@@ -195,7 +203,11 @@ ${RED_TEXT}*********************************************************************
 id_add() {
 for host in "${miners[@]}"; do 
   echo -e "${MENU}*********Miner IP: `echo $host|awk '{print $1}'`${NORMAL}"
-  ssh-copy-id -i ~/.ssh/id_rsa.pub root@$host
+  if [ `uname -n` != 'bamt-miner' ] && [ `uname -n` != 'bamt' ] && [ `uname -n` != 'smos' ]; then
+    ssh-copy-id -i ~/.ssh/id_rsa.pub root@$host
+  else
+    ssh-copy-id -i ~/.ssh/id_rsa.pub "root@$host"
+  fi
 done
 echo ""
 echo -e "${MENU}Check for errors and repeat procedure if required${NORMAL}"
@@ -511,6 +523,89 @@ control_ex
 fi
 }
 
+mailfunc() {
+get_log() {
+cat > ~/.msmtprc <<EOF	
+defaults
+    tls on
+    # the path below may need to be adjusted
+    tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account gmail
+    from none@gmail.com
+    host $smtp
+    port $port
+    auth plain
+    user $email
+
+account default : gmail
+EOF
+chmod 600 ~/.msmtprc
+echo 'set sendmail="/usr/bin/msmtp"' > ~/.mailrc	
+cat > ~/.netrc <<EOF
+machine $smtp
+    login $email
+    password $password
+EOF
+get_ip=$(cat << EOF
+#!/bin/bash
+INTERFACE=\$(cat /proc/net/arp | grep -m 1 -v Device | awk '{print $6}')
+IPL=\$(/sbin/ifconfig $INTERFACE | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1 | tr -d '[:blank:]')
+echo \$IPL > /tmp/get_ip.txt
+sed -i 's/127.0.0.1//' /tmp/get_ip.txt
+EOF
+)
+DATE=`date +%d-%b-%Y`
+echo "$DATE" > /tmp/mail.txt
+echo "" >> /tmp/mail.txt
+for host in "${miners[@]}"; do
+  echo "" >> /tmp/mail.txt
+  echo "$get_ip"|ssh root@$host 'cat - > /tmp/get_ip.sh && chmod +x /tmp/get_ip.sh && /tmp/get_ip.sh'
+  real_ip=$(ssh root@$host cat /tmp/get_ip.txt)
+  echo "********Miner IP: `echo $real_ip`" >> /tmp/mail.txt
+  output=$(ssh root@$host tail -n 5 /etc/bamt/autoRebooter.log 2>/dev/null)
+  output1=$(ssh root@$host /opt/bamt/viewgpu)
+  echo "$output1" >> /tmp/mail.txt
+  echo "" >> /tmp/mail.txt
+  echo "autoRebooter.log" >> /tmp/mail.txt
+  echo "$output" >> /tmp/mail.txt
+  unset output
+  unset output1
+  echo -n -e "${MENU} Miner IP:${NORMAL}" && echo "$real_ip logs sent to $email"
+done
+res=`cat /tmp/mail.txt`
+echo "$res" | mail -s "Mining stat" "$email"
+echo ""  
+menu_list
+}
+if [ "$notify" = 'YES' ]; then
+  dpkg -l|grep msmtp 1> /dev/null
+  if [ `echo $?` != 0 ]; then
+    echo ""
+    echo "Seems msmtp not installed, it is required for mail functionality
+You will be promted for root password to install msmtp automatically"
+    sleep 5
+    sudo apt-get --yes --force-yes install msmtp
+    clear
+    echo "Sending logs standby..."
+    echo ""
+    get_log
+  else
+    get_log
+  fi
+else
+  echo ""
+  echo -e "${ENTER_LINE}First enable email notification then you may use this feature${RED_TEXT}
+${MENU}Press [Enter] or any key to back in Main Menu${NORMAL}"
+  read -s -n1  key
+  if [ -z $key ]; then  
+    main_menu
+  else
+    main_menu
+  fi  
+fi
+}
+
 main_menu() {
 show_menu(){
 	colors
@@ -522,7 +617,8 @@ show_menu(){
     echo -e "${MENU}**${NUMBER} 5)${MENU} Switch mining software e.g. scrypt/scrypt-jane/scrypt-n.. ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 6)${MENU} Configure mining server SSH authentication ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 7)${MENU} SSH login to mining server ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 8)${MENU} Real time monitoring ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 8)${MENU} Send logs to e-mail ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 9)${MENU} Real time monitoring ${NORMAL}"
     echo -e "${MENU}*********************************************${NORMAL}"
     echo -e "${ENTER_LINE}Please pick a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
     read opt
@@ -581,10 +677,15 @@ while [ opt != '' ]
             show_menu;
             ;;   
          8) clear;
+            option_picked "Send logs to e-mail";
+            mailfunc
+            main_menu     
+            ;;
+         9) clear;
             option_picked "Real time monitoring";
             monitor
             main_menu     
-            ;;    
+            ;;     
           x)exit;
             ;;
 
@@ -603,6 +704,68 @@ main_menu
 }
 
 cron() {
+mail_cron() {
+mailrc='set sendmail="/usr/bin/msmtp"'
+netrc=$(cat << EOF
+machine $smtp
+    login $email
+    password $password
+EOF
+)	
+msmtprc=$(cat << EOF
+defaults
+    tls on
+    # the path below may need to be adjusted
+    tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account gmail
+    from none@gmail.com
+    host $smtp
+    port $port
+    auth plain
+    user $email
+
+account default : gmail
+EOF
+)
+mail_error=$(cat << EOF
+#!/bin/bash
+echo `date +%d-%b-%Y\ \ time\ %H:%M` > /tmp/mail_error.txt
+echo "" >> /tmp/mail_error.txt
+INTERFACE=\$(cat /proc/net/arp | grep -m 1 -v Device | awk '{print $6}')
+IPL=\$(/sbin/ifconfig $INTERFACE | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1 | tr -d '[:blank:]')
+echo \$IPL > /tmp/ip.txt
+sed -i 's/127.0.0.1//' /tmp/ip.txt
+IP=\$(cat /tmp/ip.txt)
+echo "----------------MINER IP: \$IP" >> /tmp/mail_error.txt
+echo "" >> /tmp/mail_error.txt
+echo "autoRebooter.log" >> /tmp/mail_error.txt
+tail -n 3 /etc/bamt/autoRebooter.log >> /tmp/mail_error.txt
+echo "" >> /tmp/mail_error.txt
+echo "viewgpu" >> /tmp/mail_error.txt
+/opt/bamt/viewgpu >> /tmp/mail_error.txt
+result=\$(cat /tmp/mail_error.txt)
+echo "\$result" | mail -s "[FAIL]Mining stat IP:\$IP" "$email"
+EOF
+)
+
+for host in "${miners[@]}"; do
+  out=$(ssh root@$host dpkg -l|grep msmtp)
+  if [ -z "$out" ]; then
+    echo "$mailrc"|ssh root@$host 'cat - > /root/.mailrc'
+    echo "$netrc"|ssh root@$host 'cat - > /root/.netrc'
+    echo "$msmtprc"|ssh root@$host 'cat - > /root/.msmtprc'
+    echo "$mail_error"|ssh root@$host 'cat - > /tmp/mail.sh && chmod +x /tmp/mail.sh'
+    ssh root@$host 'DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install msmtp bsd-mailx >/dev/null && chmod 600 /root/.msmtprc'
+  else
+    echo "$mailrc"|ssh root@$host 'cat - > /root/.mailrc'
+    echo "$netrc"|ssh root@$host 'cat - > /root/.netrc'
+    echo "$msmtprc"|ssh root@$host 'cat - > /root/.msmtprc'
+    echo "$mail_error"|ssh root@$host 'cat - > /tmp/mail.sh && chmod +x /tmp/mail.sh && chmod 600 /root/.msmtprc'
+  fi
+done
+}
+	
 if_sick () {
 for server in "${miners[@]}"; do	
 cat <<'EOF' | ssh root@$server 'cat - > /tmp/api.py && python /tmp/api.py devs > /tmp/if_sick.txt'	
@@ -703,8 +866,69 @@ fi
 EOF
 done
 }
-if_sick
-if_temp
+
+if_temp_mail() {	
+for server in "${miners[@]}"; do	
+cat <<'EOF' | ssh root@$server 'cat - > /tmp/cardcheck.sh && chmod +x /tmp/cardcheck.sh && /tmp/cardcheck.sh'
+targetMinTemp=57
+sickres="/tmp/if_sick.txt"
+i=0
+(/opt/bamt/viewgpu | awk '{ print $2; }' | cut -c -2 > /tmp/viewgpu)
+sleep 3
+array=(`cat /tmp/viewgpu`)
+if [ ${#array[@]} -eq 0 ]; then
+  echo "`date +%m-%d-%Y` `uptime | awk -F, '{sub(".*ge ",x,$1);print $1}'` viewgpu command failed to run, rebooting" >>  /etc/bamt/autoRebooter.log
+  /tmp/mail.sh
+  sync && /sbin/coldreboot &
+  sleep 30
+  echo s > /proc/sysrq-trigger
+  sleep 10
+  echo b > /proc/sysrq-trigger
+elif [ ${#array[@]} -ne 0 ]; then
+   INTERFACE=`cat /proc/net/arp | grep -m 1 -v Device | awk '{print $6}'`
+   echo "----------------MINER IP:`/sbin/ifconfig $INTERFACE | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1 | tr -d '[:blank:]'`"
+   echo "`/opt/bamt/viewgpu`"
+   echo ""
+fi
+for temp in ${array[@]}; do
+  if [ $temp -lt $targetMinTemp ]; then
+    echo "`date +%m-%d-%Y` `uptime | awk -F, '{sub(".*ge ",x,$1);print $1}'` card number $i has stopped, its current temp is $temp, coldrebooting" >> /etc/bamt/autoRebooter.log
+    /tmp/mail.sh   
+    sync && /sbin/coldreboot &
+    sleep 30
+    echo s > /proc/sysrq-trigger
+    sleep 10
+    echo b > /proc/sysrq-trigger
+  fi
+i=$(($i+1))
+done
+cards=(`cat $sickres|awk '{print $2}'`)
+egrep -w 'Sick|Dead|NoStart' $sickres
+status=`echo $?`
+if [ "$status" = 0 ]; then
+  for card in ${cards[@]}; do
+    echo "`date +%m-%d-%Y` `uptime | awk -F, '{sub(".*ge ",x,$1);print $1}'` card number $i is ${cards[$card]} , coldrebooting" >> /etc/bamt/autoRebooter.log
+    /tmp/mail.sh
+    sync && /sbin/coldreboot &
+    sleep 30
+    echo s > /proc/sysrq-trigger
+    sleep 10
+    echo b > /proc/sysrq-trigger
+  i=$(($i+1))
+  done
+fi
+EOF
+done
+}
+
+if [ "$notify" = 'YES' ]; then
+  mail_cron
+  if_sick
+  if_temp_mail
+else 
+  if_sick
+  if_temp
+fi  
 }
 
 if [ $# -eq 0 ]; then
